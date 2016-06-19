7c845c82fda956e9de2aeca3e3bd096e2275221f
Chris Hooks <chris@hooks.me.uk>
2016-06-19 19:35:21 +0100

    Add express-static with a preprocessing action

--- api/assets_controller.js
+++ api/assets_controller.js
@@ -5,6 +5,10 @@ const AssetsController = {
   create: (req, res, next) => {
     fs.createReadStream("./" + req.file.path).pipe(fs.createWriteStream('./public/' + req.file.originalname));
     res.status(201).send({path: "/public/image.jpg"});
+  },
+  showImage: (req, res, next) => {
+    console.log(req.path);
+    next();
   }
 }
 
--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 
 const ProgImage = require('./index.js');
 const multer = require('multer');
-global.app = require('express')();
+const express = require('express');
+global.app = express();
 const upload = multer({ dest: './uploads/'});
 
+app.use('/public', ProgImage.AssetsController.showImage, express.static('public'));
 app.get('/status', ProgImage.StatusController.show);
 app.post('/assets', upload.single('file'), ProgImage.AssetsController.create);
 
