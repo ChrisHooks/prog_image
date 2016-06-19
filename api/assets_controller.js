e4b9becdb05f5630d04ebf95609165db0d92d0fd
Chris Hooks <chris@hooks.me.uk>
2016-06-19 22:30:55 +0100

    Refactor more logic into the image_service

--- api/assets_controller.js
+++ api/assets_controller.js
@@ -5,35 +5,30 @@ const ImageService = require('../services/image_service.js')
 
 const AssetsController = {
   create: (req, res, next) => {
+    if(ImageService.doesImageExist(req.file.originalname)) {
+      return res.status(409).send({error: "Resource all ready exists"})
+    }
     fs.createReadStream("./" + req.file.path).pipe(fs.createWriteStream('./public/' + req.file.originalname));
     res.status(201).send({path: "/public/image.jpg"});
   },
+
   showImage: (req, res, next) => {
-    if (fs.existsSync("./public" + req.path)) {
-      next();
-      return;
+    if(ImageService.doesImageExist(req.path)) {
+      return next();
     }
 
-    fs.readdir('./public', (err, files) => {
-      if(err) {
-        res.status(500).send({ error: err });
+    ImageService.checkForOtherFormats(req.path)
+    .then((filename) => {
+      if(!!filename) {
+        let splitFilename = req.path.replace("/", "").split(".");
+        return ImageService.convertFormat(filename, splitFilename[1]);
       }
-
-      let splitFilename = req.path.replace("/", "").split(".")
-      let file = files.find((file) => {
-        return file.split(".")[0] === splitFilename[0]
-      })
-      if(!!file) {
-        ImageService.convertFormat(path.join(process.cwd(), "./public/" + file), splitFilename[1])
-        .then((image) => {
-          next();
-        })
-        .catch((e) => {
-          res.status(500).send(e)
-        })
-      } else {
-        next();
-      }
+    })
+    .then(() => {
+      next();
+    })
+    .catch((e) => {
+      res.status(500).send(e)
     })
   }
 }
--- services/image_service.js
+++ services/image_service.js
@@ -1,10 +1,39 @@
 'use-strict'
 const fs = require('fs');
+const path = require('path');
 const Promise = require('bluebird');
 const gm = Promise.promisifyAll(require('gm'));
 
 ImageService = {
+  doesImageExist: (filename) => {
+    let basename = path.basename(filename)
+      console.log(filename);
+    return fs.existsSync("./public/" + basename);
+  },
+
+  checkForOtherFormats: (filename) => {
+    let d = Promise.defer();
+    fs.readdir('./public', (err, files) => {
+      if(err) {
+        d.reject(err);
+      }
+
+      let splitFilename = path.basename(filename).split(".")
+      let file = files.find((file) => {
+        return file.split(".")[0] === splitFilename[0];
+      })
+      if(!!file) {
+        d.resolve(path.join(process.cwd(), "./public/" + file), splitFilename[1]);
+      } else {
+        d.resolve(null);
+      }
+    })
+    return d.promise;
+  },
+
   convertFormat: (filename, format) => {
+    console.log(filename);
+    console.log(format);
     var writeStream = fs.createWriteStream(filename.split(".")[0] + '.' + format)
     var convertedImageStream = gm(filename).setFormat(format).stream();
     var d = Promise.defer();
